089f99b5b8f2f23aa40ec772b9d27c5e704975e9
furuyaatsushi <sdvxmnn@gmail.com>
2020-02-18 23:32:43 +0900

    Update chap3_7_3.php

---
+++ site/chap3_7_3.php
@@ -0,0 +1,10 @@
+<?php
+$team = ["A", "B", "C", "D", "E"];
+$opps = ["A", "B", "C", "D", "E"];
+foreach($team as $t1){
+  array_shift($opps);
+  foreach($opps as $t2){
+    var_dump($t1 . "vs" . $t2);
+  }
+}
+?>
