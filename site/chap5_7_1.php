5f996be85a2181fe34912e237b31b9fdf3648b30
furuyaatsushi <sdvxmnn@gmail.com>
2020-02-29 17:21:33 +0900

    set function

--- site/chap5_7_1.php
+++ site/chap5_7_1.php
@@ -1,3 +1,9 @@
 <?php
-
+function h($str){
+  return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
+}
+function eto($year){
+  $etos = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"];
+  return $etos[($years - 4) % 12];
+}
 ?>
