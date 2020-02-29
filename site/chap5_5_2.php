872be98de83d10885be3388f0fc95bda77a66f3f
furuyaatsushi <sdvxmnn@gmail.com>
2020-02-29 16:42:19 +0900

    Create chap5_5_2.php

---
+++ site/chap5_5_2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>サンプルプログラム</h1>
+    <form action="chap5_5_post.php" method="post">
+    <input type="text" name="comment">
+    <input type="submit" value="送信">
+    </form>
+  </body>
+</html>
